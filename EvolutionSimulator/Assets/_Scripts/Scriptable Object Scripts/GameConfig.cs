15eeea1ee542b7f1465dab5a21302274985b412d
yuriiihor <hefestsbunker@gmail.com>
2020-05-25 23:38:26 +0300

    Mouse spawning

---
+++ EvolutionSimulator/Assets/_Scripts/Scriptable Object Scripts/GameConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "_GameConfig", menuName = "Data/GameConfig")]
+public class GameConfig : ScriptableObject
+{
+    public Mouse mouse1prefab;
+    public Mouse mouse2prefab;
+
+    public int mouse1count = 10;
+    public int mouse2count = 10;
+}
--- EvolutionSimulator/Assets/_Scripts/_EvolutionProccessManager.cs
+++ EvolutionSimulator/Assets/_Scripts/_EvolutionProccessManager.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class _EvolutionProccessManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameConfig gameConfig;
+    public int currentTurn = 0;
+
+    public List<GameObject> mouse1spawnPoses;
+    public List<GameObject> mouse2spawnPoses;
+
+    public GameObject root;
+    
     void Start()
     {
-        
+        SpawnMouses(gameConfig.mouse1prefab, gameConfig.mouse1count, mouse1spawnPoses);
+        SpawnMouses(gameConfig.mouse2prefab, gameConfig.mouse2count, mouse2spawnPoses);
     }
 
-    // Update is called once per frame
-    void Update()
+    void SpawnMouses(Mouse mousePrefab, int count, List<GameObject> spawnPoses)
     {
-        
+        for(int i = 0; i < count; i++)
+        {
+            Vector3 spawnPosition = spawnPoses[i].transform.position;
+            var newMouse = Instantiate(mousePrefab, spawnPosition, Quaternion.identity);  
+            newMouse.transform.parent = root.transform;
+        }
     }
+
 }
